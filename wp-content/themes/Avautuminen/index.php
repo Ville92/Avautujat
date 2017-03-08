21203bbc3bd6b373518e4ca66d679df90e6aa8f9
Ville Aho <Ville@Villes-MBP.home>
2017-03-08 17:56:29 +0200

    Custom sivu hakutuloksille ja turhat formit pois avautumisista

--- wp-content/themes/Avautuminen/index.php
+++ wp-content/themes/Avautuminen/index.php
@@ -1,38 +1,19 @@
 <?php get_header(); ?>
 
-
 <div class="content-row">
     <main>
-        <h1><?php the_category(); ?></h1>
-        <h5>Ahdistaako? Tekeekö mieli avautua? Älä nolaa itseäsi facebookissa vaan avaudu täällä anonyymisti!</h5>
-        
-        <!--SUBMIT POST-->
-        <?php include_once("submit-post-form.php");?>
-        
-        <h3 class="uudet-ja-suositut">
-            <a href="<?php echo get_home_url(); ?>">&#9679; Uusimmat</a>
-            <a href="<?php echo get_home_url(); ?>/?page_id=187">	
-&#9733; Suosituimmat</a>
-        </h3>
-        
         <?php if (have_posts()): ?>
             <?php while(have_posts()): ?>
+        
                 <?php the_post(); ?>
-        <a class="article-link" href="<?php echo get_post_permalink( $id, $leavename, $sample ); ?>">
-                <h2><?php the_title(); ?></h2>
+                <h1><?php the_title(); ?></h1>
                 <?php the_content(); ?>
                 <?php the_post_thumbnail(); ?>
                 <?php comments_template(); ?> 
-                <img class="commenting" src="<?php echo get_template_directory_uri(); ?>/img/kommentti.png" height="16"><a href="<?php echo get_post_permalink( $id, $leavename, $sample ); ?>"><?php comments_number( "0 kommenttia", "1 kommentti"); ?></a>
-
                 <?php echo the_category( $separator, $parents, $artikkeli['ID'] ); ?>
-
-
-        </a>
-            
+        
             <?php endwhile; ?>
         <?php endif; ?>
-        
     </main>
 </div>
 
